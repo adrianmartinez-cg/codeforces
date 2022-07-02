d3ecc070b1ff5a4b50892e47b61cb8c5ddd523fb
Pedro Adrian <pedro.martinez@ccc.ufcg.edu.br>
2022-07-02 13:28:13 -0300

    Trying another approach in problem A, started problem B

--- src/DivideThreeMultTwo.java
+++ src/DivideThreeMultTwo.java
@@ -1,3 +1,4 @@
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -21,10 +22,10 @@ public class DivideThreeMultTwo {
         long[] array = Arrays.stream(arrayNumsStr)
                              .mapToLong(Long::parseLong)
                              .toArray();
-        long[] answer = solve(n,array);
+        long[] answer = solveA(n,array);
         showAnswer(answer);
     }
-    private static long[] solve(int n, long[] array){
+    private static long[] solveA(int n, long[] array){
         long[] answer = new long[n];
         long[] predecessors = new long[n];
         int first = 0;
@@ -45,7 +46,7 @@ public class DivideThreeMultTwo {
                 predecessors[i] = -1;
             }
         }
-        
+
         int nextAnswerIndex = 1;
         int nextPredecessorToFind = first;
 
@@ -60,6 +61,20 @@ public class DivideThreeMultTwo {
             }
         }
         return answer;
+    }
+
+    private static Long[] solve(int n, long[] array){
+        LinkedList<Long> answer = new LinkedList<>();
+        for(int i=0; i<n;i++){
+            for(int j=0; j<n; j++){
+                if(i != j){
+                    if (array[i] == array[j] * 2 || array[i] == array[j] / 3){
+
+                    }
+                }
+            }
+        }
+        return answer.toArray(new Long[0]);
     }
 
     private static void showAnswer(long[] array){
